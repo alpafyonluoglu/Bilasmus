ee1ff8b06a90f75f4b0a21f72cf0e2ec5f5215f1
Alp Afyonluoğlu <alpafyonluoglu@gmail.com>
2022-12-12 20:50:51 +0300

    Fixed error handling issue

--- backend/app.js
+++ backend/app.js
@@ -69,7 +69,7 @@ app.use((req, res, next) => {
     return next(createError(404));
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => { // Do not remove "next" param
     let statusCode = err.status || 500;
     let obj = {
         code: statusCode,
