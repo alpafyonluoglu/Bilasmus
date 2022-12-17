89c9ae2465cc10e2fb769ac9e71e367c12ee79d5
SelinBaharGundogar <bahar.gundogar@ug.bilkent.edu.tr>
2022-12-17 20:40:54 +0300

    Instructor main page update

---
+++ frontend/Instructor/instructorMainPage.js
@@ -0,0 +1,16 @@
+/*
+    Methods for Instructor Main Page
+*/
+function rejectCourse(){
+    //backend reject 
+}
+function approveCourse(){
+    //backend approve
+}
+
+//if there are not course requests to show
+if ( true ){
+    
+}
+
+//if there are course requests
