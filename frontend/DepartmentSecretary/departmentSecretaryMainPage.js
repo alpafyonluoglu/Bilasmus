329271d9c6f989fa3c69cc8347380f53b863e47d
SelinBaharGundogar <bahar.gundogar@ug.bilkent.edu.tr>
2022-12-17 14:12:15 +0300

    Changed assets file

---
+++ frontend/DepartmentSecretary/departmentSecretaryMainPage.js
@@ -0,0 +1,12 @@
+/*
+    Methods for the Department Secretary Main Page
+*/
+
+//Method to approve courses
+function approveCourse(){
+    //backend connection
+}
+//Method to reject courses
+function rejectCourse(){
+    //backend connection
+}
