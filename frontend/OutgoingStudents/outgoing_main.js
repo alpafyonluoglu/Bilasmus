f97e58f95cf5d61ec9e11a4193ccd156c3561fcf
Erkin-Aydin <erkinresearch@gmail.com>
2022-12-17 17:25:15 +0300

    progress bar changes

--- frontend/OutgoingStudents/outgoing_main.js
+++ frontend/OutgoingStudents/outgoing_main.js
@@ -3,7 +3,7 @@ function openLink() {
 }
 
 function calcPercentage() {
-    let check1 = document.getElementById("formCheck-1").checked;
+    let check1 = document.getElementById("formCheck-1").target().checked();
     let check2 = document.getElementById("formCheck-2").checked;
     let check3 = document.getElementById("formCheck-3").checked;
     let check4 = document.getElementById("formCheck-4").checked;
