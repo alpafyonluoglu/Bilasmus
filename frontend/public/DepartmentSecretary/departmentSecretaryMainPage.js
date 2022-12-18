9c32540a078ea0386c0d300d61396d36838461c4
SelinBaharGundogar <bahar.gundogar@ug.bilkent.edu.tr>
2022-12-18 18:41:10 +0300

    DS main page data table change

--- frontend/public/DepartmentSecretary/departmentSecretaryMainPage.js
+++ frontend/public/DepartmentSecretary/departmentSecretaryMainPage.js
@@ -13,12 +13,26 @@ document.getElementById("table").innerHTML = ' <tr>'+
 '<td id="studentName">India</td>'+
 '<td id="courseName">Souvik Kundu</td>'+
 '<td><button class="btn btn-primary" id="approveCourseButton" onclick="approveCourse()" type="button" style="background-color: var(--bs-green);margin-left: 1rem;height: 1.5rem;padding-top: 0px;">Approve</button><button class="btn btn-primary" id="rejectCourseButton" onclick="rejectCourse()" type="button" style="background-color: var(--bs-red);margin-left: 1rem;height: 1.5rem;padding-top: 0px;">Reject</button></td>'+
+'</tr>'+
+' <tr>'+
+'<td id="ID">01</td>'+
+'<td id="studentName">India</td>'+
+'<td id="courseName">Souvik Kundu</td>'+
+'<td><button class="btn btn-primary" id="approveCourseButton" onclick="approveCourse()" type="button" style="background-color: var(--bs-green);margin-left: 1rem;height: 1.5rem;padding-top: 0px;">Approve</button><button class="btn btn-primary" id="rejectCourseButton" onclick="rejectCourse()" type="button" style="background-color: var(--bs-red);margin-left: 1rem;height: 1.5rem;padding-top: 0px;">Reject</button></td>'+
 '</tr>';
 //Method to approve courses
 function approveCourse(){
     //backend connection
+    console.log("approve button works");
 }
+
 //Method to reject courses
 function rejectCourse(){
     //backend connection
+    console.log("reject button works");
 }
+//getting student ID value
+let studentIDApprovePressed = document.getElementById("approveCourseButton").closest('tr').cells[0].textContent;
+let studentIDRejectPressed = document.getElementById("approveCourseButton").closest('tr').cells[0].textContent;
+
+//Backend code
