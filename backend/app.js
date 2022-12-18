a6945185d53d6ed36a7078a554c39fb068c0a677
Alp Afyonluoğlu <alpafyonluoglu@gmail.com>
2022-12-18 17:52:21 +0300

    Updated CORS added Cookie headers

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
     credentials: true,
     origin: "https://bilasmus-app.web.app",
-    allowedHeaders: ["Content-Type", "*"],
+    allowedHeaders: ["Content-Type", "Cookie", "*"],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
