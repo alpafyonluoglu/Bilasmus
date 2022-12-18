3912889731cc097b0c2a287aa34118dbc561bbb4
Alp Afyonluoğlu <alpafyonluoglu@gmail.com>
2022-12-19 00:27:53 +0300

    Added endpoint to get current user's file

--- backend/routes/FileSystemRouter.js
+++ backend/routes/FileSystemRouter.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const Multer = require("multer");
 const util = require("util");
 const gcpFileStorageController = require("../controllers/GcpFileStorageController");
+const fileStorageController = require("../controllers/FileStorageController");
 const databaseController = require("../controllers/DatabaseController");
 const Document = require("../models/Document");
+const userController = require("../controllers/UserController");
 
 class FileSystemRouter {
   /*
   - Upload: POST /file/upload (params: file & type)
+  - Get current user's file: GET /file/:type/this
    */
   processFile;
 
@@ -47,6 +50,7 @@ class FileSystemRouter {
           }
 
           // Add to DB
+          let originalResult = result;
           let now = new Date();
 
           let doc = new Document();
@@ -57,16 +61,38 @@ class FileSystemRouter {
               return next(result);
             }
 
-            return res.json({
-              code: 200,
-              uploaded: true
-            });
+            originalResult.code = 200;
+            return res.json(originalResult);
           });
         })
       }
       catch (error) {
         return next(createError(500, "File could not be uploaded"))
       }
+    });
+
+    router.get('/:type/this', (req, res, next) => {
+      // Check session
+      if (!req.session || !req.session.userID) {
+        return next(createError(401));
+      }
+
+      // Check params
+      if (!DOCUMENT.TYPES.includes(req.params.type)) {
+        return next(createError(400, "Unknown document type"))
+      }
+
+      // Call controller
+      let documentType = req.params.type;
+      let userId = req.session.userID;
+      fileStorageController.getFiles(documentType, userId, (result) => {
+        if (result instanceof Error) {
+          return next(result);
+        }
+
+        result.code = 200;
+        return res.json(result);
+      });
     });
 
     return router;
