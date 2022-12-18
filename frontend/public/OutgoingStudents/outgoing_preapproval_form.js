75d2e7d3f217d79e6fbe42572ccab3675d2b7d11
Erkin-Aydin <erkinresearch@gmail.com>
2022-12-18 15:26:49 +0300

    klk

--- frontend/public/OutgoingStudents/outgoing_preapproval_form.js
+++ frontend/public/OutgoingStudents/outgoing_preapproval_form.js
@@ -1,7 +1,17 @@
 let uploadFileButton = document.getElementById("upload_file");
-
+let uploadArea = document.getElementById("upload");
 uploadFileButton.addEventListener('click', uploadFile)
 
 function uploadFile() {
-    console.log("upload file");
+    //Upload: POST /file/upload (params: file)
+    //https://bilasmus.uc.r.appspot.com/file/upload
+    /*
+        Document types:
+        - cr: Course request
+        - la: Learning agreement
+        - lp: Language proficiency
+        - pa: Pre approval
+        - t: Transcript
+     */
+    uploadArea.getFile();
 }
